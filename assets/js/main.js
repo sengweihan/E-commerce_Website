47fcf556b224be88044c38ab74aabb8cf17452ca
wsen0004 <wsen0004@student.monash.edu>
2023-07-27 14:11:45 +0800

    Added event listener to the add-to-cart button

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,23 @@ heart_icon.forEach((icon) => {
     }
   });
 });
+
+// QUERY SELECTOR FOR ADD TO CART BUTTON.
+const cart_btn = document.querySelectorAll(
+  ".new-arrival-item .add-cart button"
+);
+cart_btn.forEach((btn) => {
+  btn.addEventListener("mouseover", (event) => {
+    event.target.innerHTML = "<i class='bx bxs-cart'></i> Add to cart";
+    event.target.style.backgroundColor = "white";
+    event.target.style.color = "#dc2626";
+  });
+});
+
+cart_btn.forEach((btn) => {
+  btn.addEventListener("mouseout", (event) => {
+    event.target.innerHTML = "Add to cart";
+    event.target.style.backgroundColor = "#dc2626";
+    event.target.style.color = "white";
+  });
+});
